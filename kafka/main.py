ff536491627d6f3a1db5887b58816ed031bcc3d0
Mateus Souza <mateus_msouza@outlook.com>
2025-11-07 20:31:08 +0000

    update

--- kafka/main.py
+++ kafka/main.py
@@ -1,8 +1,10 @@
 from confluent_kafka import Producer
 
-producer = Producer(bootstrap_servers='localhost:9092', acks='all')
+config = {'bootstrap.servers': 'localhost:9092'}
+
+producer = Producer(config)
 for _ in range(100):
-    producer.send('foobar', b'some_message_bytes')
+    producer.produce('created_events', b'some_message_bytes')
 
 print("sending all messages to kafka")
 print(f"sending all messages to kafka")
